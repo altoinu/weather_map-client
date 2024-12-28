573bbd9909ac965e4371487eb1cd23d9fa72d01f
Kaoru Kawashima <8354804+altoinu@users.noreply.github.com>
2024-12-28 11:21:10 -0500

    hooks for weather api

---
+++ app/_hooks/useAPIGet5DayWeather.ts
@@ -0,0 +1,8 @@
+import useFetch from "./useFetch";
+
+export default function useAPIGet5DayWeather() {
+  return useFetch({
+    method: "GET",
+    url: "http://localhost:4000/5DayWeather.json",
+  });
+}

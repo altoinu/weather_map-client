23e9c8e6a0692fcadcc4c1cc5a3d59886e0abb9c
Kaoru Kawashima <8354804+altoinu@users.noreply.github.com>
2025-02-19 13:10:38 -0500

    eslint config update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,13 @@
 import { FlatCompat } from "@eslint/eslintrc";
+import pluginJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 import eslintPluginJest from "eslint-plugin-jest";
 import eslintPluginJestDom from "eslint-plugin-jest-dom";
+import pluginReact from "eslint-plugin-react";
 import eslintPluginReactHooks from "eslint-plugin-react-hooks";
+import globals from "globals";
 import { dirname } from "path";
+import tseslint from "typescript-eslint";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -20,7 +24,39 @@ const eslintConfig = [
       "react-hooks": eslintPluginReactHooks,
     },
     rules: eslintPluginReactHooks.configs.recommended.rules,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
   },
+  {
+    // for jest test files
+    files: [
+      "**/*.test.{js,cjs,mjs,jsx,ts,tsx}",
+      "**/__tests__/**/*.{js,cjs,mjs,jsx,ts,tsx}",
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+        fetchMock: "readonly",
+      },
+    },
+  },
+  {
+    // for other non test files
+    files: ["**/*.{js,cjs,mjs,jsx,ts,tsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"],
   eslintPluginJest.configs["flat/recommended"],
   eslintPluginJestDom.configs["flat/recommended"],
   eslintConfigPrettier,

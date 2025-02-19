c387864986c415bc125437663423bdd4a414be63
Kaoru Kawashima <8354804+altoinu@users.noreply.github.com>
2025-02-19 13:31:17 -0500

    useFetch jsdoc update

--- app/_hooks/useFetch.mjs
+++ app/_hooks/useFetch.mjs
@@ -1,8 +1,17 @@
-"use client";
-
 import HTTPError from "../_types/HTTPError";
 import { useCallback, useState } from "react";
 
+/**
+ * Fetch status.
+ * @memberof module:useFetch
+ * @alias module:useFetch.FetchStatus
+ * @readonly
+ * @enum
+ * @property {string} Idle
+ * @property {string} Pending
+ * @property {string} Succeeded
+ * @property {string} Failed
+ */
 export var FetchStatus;
 (function (FetchStatus) {
   FetchStatus[(FetchStatus["Idle"] = 0)] = "Idle";
@@ -12,105 +21,220 @@ export var FetchStatus;
 })(FetchStatus || (FetchStatus = {}));
 
 /**
- * Custom hook that wraps `fetch` and handles all request & response handling.
- *
- * @param options Default request object data.
- * @returns An object containing:
- *   - `fetch`: The method that will perform the request & return a Promise.
- *   - `data`: The response body object of the most recent request.
- *   - `fetchStatus: Current FetchStatus
- *   - `isFetching`: Boolean; true when the network request is active.
- *   - `response`: The HTTP Response object.
+ * Union type of supported HTTP method strings
+ * @typedef {("DELETE"|"GET"|"PATCH"|"POST"|"PUT")} HTTPMethod
+ * @memberof module:useFetch
+ * @alias module:useFetch.HTTPMethod
  */
-export default function useFetch({ headers, method = "GET", url }) {
+
+/**
+ * @typedef {Object} FetchArgs
+ * @memberof module:useFetch
+ * @alias module:useFetch.FetchArgs
+ */
+
+/**
+ * @typedef {(JSON|string|Blob)} FetchResponseData
+ * @memberof module:useFetch
+ * @alias module:useFetch.FetchResponseData
+ */
+
+/**
+ * @typedef {Object} FetchResponse
+ * @memberof module:useFetch
+ * @alias module:useFetch.FetchResponse
+ * @property {FetchResponseData} data The response body object.
+ * @property {Response} response The HTTP Response object.
+ */
+
+/**
+ * @typedef {Promise<FetchResponse>} FetchPromise
+ * @memberof module:useFetch
+ * @alias module:useFetch.FetchPromise
+ */
+
+/**
+ * @typedef {function(FetchArgs):FetchPromise} FetchMethod
+ * @memberof module:useFetch
+ * @alias module:useFetch.FetchMethod
+ * @async
+ * @param {FetchArgs} options
+ * @returns {FetchPromise}
+ */
+
+/**
+ * @typedef {Object} UseFetchArgs
+ * @memberof module:useFetch
+ * @alias module:useFetch.UseFetchArgs
+ */
+
+/**
+ * @typedef {Object} useFetchReturnObject
+ * @memberof module:useFetch
+ * @alias module:useFetch.useFetchReturnObject
+ * @property {FetchMethod} fetch The method that will perform the request and
+ * return a Promise.
+ * @property {(FetchResponseData|null|undefined)} data The response body object
+ * of the most recent request.
+ * @property {(Error|unknown)} error Error object, if error had occurred.
+ * @property {FetchStatus} fetchStatus Current FetchStatus.
+ * @property {boolean} isFetching true when the network request is active.
+ * @property {(Response|null)} response The HTTP Response object.
+ */
+
+/**
+ * Custom hook that wraps `fetch` and handles all request & response handling.
+ * @module useFetch
+ * @version 1.0.0 2025/02/16
+ * @param {UseFetchArgs} options Default request object data.
+ * @param {Headers} [options.headers] HTTP request headers
+ * @param {HTTPMethod} [options.method="GET"] HTTP method string
+ * @param {string} options.url Request URL string
+ * @returns {useFetchReturnObject} An object containing fetch method and
+ * other fetch operation related data.
+ * @example
+ * const { fetch, data } = useFetch();
+ *
+ * // async await waty to fetch
+ * async function doFetch() {
+ *  try {
+ *    const value = await fetch();
+ *    console.log("fetch success!", value);
+ *  } catch (error) {
+ *    console.log("fetch error", error);
+ *  }
+ * }
+ *
+ * doFetch();
+ *
+ *
+ *
+ * // then catch way to fetch
+ * fetch().then((value) => {
+ *  console.log("fetch success!", value);
+ * }).catch((error) => {
+ *  console.log("fetch error", error);
+ * });
+ *
+ *
+ *
+ * // Fetch via useEffect
+ * useEffect(() => {
+ *  if (fetch) {
+ *    fetch();
+ *  }
+ * }, [fetch]);
+ *
+ * useEffect(() => {
+ *  if (data) {
+ *    console.log("data update:", data);
+ *  }
+ * }, [data]);
+ */
+function useFetch({ headers, method = "GET", url }) {
   const [data, setData] = useState();
   const [error, setError] = useState();
   const [fetchStatus, setFetchStatus] = useState(FetchStatus.Idle);
   const [isFetching, setIsFetching] = useState(false);
   const [response, setResponse] = useState();
 
+  /**
+   * The method to perform the request & return a Promise.
+   * @memberof module:useFetch
+   * @alias module:useFetch.fetch
+   * @async
+   * @function fetch
+   * @inner
+   * @param {FetchArgs} options
+   * @param {(string|ArrayBuffer|DataView|Blob|File|URLSearchParams|FormData|ReadableStream)} [options.body]
+   * Request body data to be sent with this fetch operation
+   * @param {Headers} [options.headers] HTTP request headers to be used, this time only
+   * @param {(string|URLSearchParams|string[][]|Record<string, string>)} [options.query]
+   * Query string params to append to the request URL
+   * @param {string} [options.url] Request URL to be used, this time only
+   * @returns {FetchPromise}
+   */
   const executeFetch = useCallback(
-    async ({ body, headers: newHeaders, query, url: newUrl } = {}) => {
+    async ({
+      body,
+      headers: overrideHeaders,
+      query,
+      url: overrideUrl,
+    } = {}) => {
       setData(undefined);
       setError(null);
       setFetchStatus(FetchStatus.Pending);
       setIsFetching(true);
       setResponse(null);
 
-      return new Promise(async (resolve, reject) => {
-        // if newHeaders is specified, use that instead for this execute only
-        const requestHeaders = newHeaders || headers;
-        // if newUrl is specified, use that instead for this execute only
-        let requestUrl = newUrl || url;
-        // Append query string to request URL, if provided.
-        if (query) {
-          const queryParams = new URLSearchParams(query);
-          requestUrl += `?${queryParams}`;
-        }
+      const requestHeaders = overrideHeaders || headers;
+      let requestUrl = overrideUrl || url;
 
-        const request = new Request(requestUrl, {
-          headers: requestHeaders,
-          method,
-          body,
-        });
+      if (query) {
+        const queryParams = new URLSearchParams(query);
+        requestUrl += `?${queryParams}`;
+      }
 
-        try {
-          const response = await fetch(request);
-
-          if (!response.ok) {
-            throw new HTTPError(method, requestUrl, response.status, response);
-          }
-
-          let responseData = null;
-
-          try {
-            const contentType = response.headers.get("Content-Type");
-
-            if (contentType?.includes("application/json")) {
-              responseData = await response.json();
-            } else if (contentType?.includes("text/plain")) {
-              responseData = await response.text();
-            } else {
-              responseData = await response.blob();
-            }
-          } catch (error) {
-            // if content-length is non-empty, then something is wrong
-            if (response.headers.get("Content-Length")) {
-              throw error;
-            }
-          }
-
-          setData(responseData);
-          setError(null);
-          setFetchStatus(FetchStatus.Succeeded);
-          setIsFetching(false);
-          setResponse(response);
-
-          resolve({ data: responseData, response });
-        } catch (error) {
-          setData(null);
-          setError(error);
-          setFetchStatus(FetchStatus.Failed);
-          setIsFetching(false);
-
-          if (error instanceof Error) {
-            if (error instanceof HTTPError) {
-              console.error(error);
-              setResponse(error.response);
-            } else {
-              console.error(`${method} ${requestUrl}: ${error.message}`);
-              setResponse(null);
-            }
-            /*
-            else {
-              console.error(`${method} ${requestUrl}`, error);
-            }
-            */
-          }
-
-          reject(error);
-        }
+      const request = new Request(requestUrl, {
+        headers: requestHeaders,
+        method,
+        body,
       });
+
+      try {
+        const response = await fetch(request);
+
+        if (!response.ok) {
+          throw new HTTPError(method, requestUrl, response.status, response);
+        }
+
+        const contentType = response.headers.get("Content-Type");
+        let responseData = null;
+        try {
+          if (contentType?.includes("application/json")) {
+            responseData = await response.json();
+          } else if (contentType?.includes("text/plain")) {
+            responseData = await response.text();
+          } else {
+            responseData = await response.blob();
+          }
+        } catch (error) {
+          // if content-length is non-empty, then something is wrong
+          if (response.headers.get("Content-Length")) {
+            throw error;
+          }
+        }
+
+        setData(responseData);
+        setError(null);
+        setFetchStatus(FetchStatus.Succeeded);
+        setIsFetching(false);
+        setResponse(response);
+
+        return Promise.resolve({ data: responseData, response });
+      } catch (error) {
+        setData(null);
+        setError(error);
+        setFetchStatus(FetchStatus.Failed);
+        setIsFetching(false);
+
+        if (error instanceof Error) {
+          if (error instanceof HTTPError) {
+            console.error(error);
+            setResponse(error.response);
+          } else {
+            console.error(`${method} ${requestUrl}: ${error.message}`);
+            setResponse(null);
+          }
+          /*
+          else {
+            console.error(`${method} ${requestUrl}`, error);
+          }
+          */
+        }
+
+        return Promise.reject(error);
+      }
     },
     [headers, method, url],
   );
@@ -118,9 +242,11 @@ export default function useFetch({ headers, method = "GET", url }) {
   return {
     fetch: executeFetch,
     data,
+    error,
     fetchStatus,
     isFetching,
     response,
-    error,
   };
 }
+
+export default useFetch;
